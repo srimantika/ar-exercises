7895acd6ea1e572f6287bfdcc802d064c6438ae0
srimantika <srimantika@gmail.com>
2022-01-03 22:15:42 +0000

    Calculations of sum,average and count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@sum = Store.sum(:annual_revenue)
+@aver = Store.average(:annual_revenue)
+
+@mill = Store.where("annual_revenue > 1000000").count
+
+puts @sum
+puts @aver
+puts @mill
