fd7ffda1dc085f9d8a22fce6a79d7e2c1e937513
srimantika <srimantika@gmail.com>
2022-01-03 22:43:57 +0000

    add validations for both stores and employees

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "What store name would you like to add?"
+store_name = gets.chomp
+
+insert = Store.create(name: store_name)
+
+puts insert.errors.full_messages
